8fa2aa04181350b9b6808b45aaf6fce764f5f759
Fabian Vis <70572501+fabian-vis@users.noreply.github.com>
2022-02-18 10:57:39 +0100

    WAFS changes

--- spa/JS/script.js
+++ spa/JS/script.js
@@ -17,17 +17,63 @@ clearIcon.addEventListener("click", () => {
 })
 
 
+
 /* Fetching data van de API */
 const testSection = document.querySelector('#test');
+const vliegSection = document.querySelector('main > section');
+const headerSection = document.querySelector('header');
+
+function displayLoading() {
+    testSection.classList.add("skeleton");
+    // na een tijdje gaat de loading state automatisch uit 
+    setTimeout(() => {
+
+        testSection.classList.remove("skeleton");
+
+    }, 5000);
+}
+
+function hideLoading() {
+    testSection.classList.remove("skeleton");
+}
+
+function inVliegen() {
+    vliegSection.classList.add("vliegen");
+}
+
+function headerOpacity() {
+    headerSection.classList.add("headerOpacity");
+}
+
+
+
+displayLoading()
+
 
 const getAndRenderData = () => {
+
+
     const getURL = 'https://www.rijksmuseum.nl/api/nl/collection?key=8op6V3T9&involvedMaker=Rembrandt+van+Rijn'
-    fetch(getURL).then(response => response.json())
+    fetch(getURL)
         .then(response => {
+            console.log(response)
+            if (response.status >= 200 && response.status <= 299) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(response => {
+            hideLoading()
+            inVliegen()
+            headerOpacity()
             console.log(response.artObjects)
             response.artObjects.forEach(kunst => {
-                testSection.insertAdjacentHTML('afterbegin', `<article><p>${kunst.title}</p> <img src="${kunst.webImage.url}"></article>`)
+                testSection.insertAdjacentHTML('afterbegin', `<article><p>${kunst.title}</p> <img class="skeleton" src="${kunst.webImage.url.slice(0, -3)+"=s1000"}"></article>`)
             })
-        }).catch(error => document.body.insertAdjacentHTML('beforebegin', error))
+        })
+        .catch(error => testSection.insertAdjacentHTML('afterbegin', `werkt niet neef`))
+
+
 }
 getAndRenderData()
